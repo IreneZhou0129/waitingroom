d76674d79804eb1f559555bc4ec4c4d19ed62f54
Tien Phan <fanngoctien@gmail.com>
2018-02-17 18:34:42 -0500

    change

--- app/src/main/java/uottahack/waiting_room/MainActivity.java
+++ app/src/main/java/uottahack/waiting_room/MainActivity.java
@@ -2,6 +2,9 @@ package uottahack.waiting_room;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    private void setContentsOfTextView(int id, String newContents) {
+        View v = findViewById(id);
+        TextView textView = (TextView) v;
+        textView.setText(newContents);
+    }
+
+    private String getInputOfTextField(int id) {
+        View v = findViewById(id);
+        EditText editText = (EditText) v;
+        String input = editText.getText().toString();
+        return input;
     }
 }
