da72e17c5cf322c86ee372017c43b3994aa13c64
dhruvitb <dnbhavsa@uwaterloo.ca>
2018-02-18 02:08:21 -0500

    working qr scanner

--- app/src/main/java/uottahack/waiting_room/MainActivity.java
+++ app/src/main/java/uottahack/waiting_room/MainActivity.java
@@ -1,28 +1,54 @@
 package uottahack.waiting_room;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
+import android.widget.Button;
 import android.widget.TextView;
+
+import com.google.android.gms.vision.barcode.Barcode;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button scanButton;
+    TextView result;
+    public static final int REQUEST_CODE = 100;
+    public static final int PERMISSION_REQUEST = 200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    private void setContentsOfTextView(int id, String newContents) {
-        View v = findViewById(id);
-        TextView textView = (TextView) v;
-        textView.setText(newContents);
+        scanButton = findViewById(R.id.QR);
+        result = findViewById(R.id.result);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMISSION_REQUEST);
+        }
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CodeScanner.class);
+                startActivityForResult(intent, REQUEST_CODE);
+            }
+        });
     }
 
-    private String getInputOfTextField(int id) {
-        View v = findViewById(id);
-        EditText editText = (EditText) v;
-        String input = editText.getText().toString();
-        return input;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK) {
+            if (data != null) {
+                final Barcode barcode = data.getParcelableExtra("barcode");
+                result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        result.setText(barcode.displayValue);
+                    }
+                });
+            }
+        }
     }
 }
