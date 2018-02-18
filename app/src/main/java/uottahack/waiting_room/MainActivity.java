ebccdb3f139f2eba35e1b30746b28b007c7bebb3
YasminA1998 <yasminfathi90@gmail.com>
2018-02-17 23:53:14 -0500

    Yasmin Vibe

--- app/src/main/java/uottahack/waiting_room/MainActivity.java
+++ app/src/main/java/uottahack/waiting_room/MainActivity.java
@@ -1,22 +1,30 @@
 package uottahack.waiting_room;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.os.Vibrator;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        // Vibrate for 500 milliseconds
+        v.vibrate(500);
     }
     private void setContentsOfTextView(int id, String newContents) {
         View v = findViewById(id);
         TextView textView = (TextView) v;
         textView.setText(newContents);
+
     }
 
     private String getInputOfTextField(int id) {
